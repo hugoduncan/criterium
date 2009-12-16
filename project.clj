84b80016570a59a368d85a2b6870500eb9c3c20d
Hugo Duncan <hugo_duncan@yahoo.com>
2009-12-16 11:34:42 -0500

    Fixed leiningen project description, and added rudimentary install instructions

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
-(defproject criterium "1.0.0-SNAPSHOT"
+(defproject criterium "0.0.1-SNAPSHOT"
   :description "benchmarking library"
-  :dependencies [[org.clojure/clojure "1.1.0-new-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.1.0-new-SNAPSHOT"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
