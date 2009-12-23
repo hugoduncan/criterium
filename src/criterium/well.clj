b3c59a3344f91e89c7d1bdc6d9b1fe6c845b421a
Hugo Duncan <hugo_duncan@yahoo.com>
2009-12-23 15:40:54 -0500

    Added copyright to source files

--- src/criterium/well.clj
+++ src/criterium/well.clj
@@ -1,3 +1,12 @@
+;;;; Copyright (c) Hugo Duncan. All rights reserved.
+
+;;;; The use and distribution terms for this software are covered by the
+;;;; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;;;; which can be found in the file epl-v10.html at the root of this distribution.
+;;;; By using this software in any fashion, you are agreeing to be bound by
+;;;; the terms of this license.
+;;;; You must not remove this notice, or any other, from this software.
+
 ;;;; Improved Long-Period Generators Based on Linear Recurrences Modulo 2, F. Panneton, P. L'Ecuyer and M. Matsumoto
 ;;;; http://www.iro.umontreal.ca/~panneton/WELLRNG.html
 
