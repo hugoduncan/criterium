09b74c083b35b16ce6947ca1fcad02c6cab90207
Hugo Duncan <hugo@hugoduncan.org>
2011-04-26 22:50:58 -0400

    Update version for release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject criterium "0.0.1-SNAPSHOT"
+(defproject criterium "0.1.0"
   :description "Benchmarking library"
   :url "http://github.com/hugoduncan/criterium"
   :dependencies [[org.clojure/clojure "1.2.0"]]
@@ -7,6 +7,6 @@
                      [autodoc "0.7.1"]]
   :autodoc {:name "Criterium"
             :description "A benchmarking library."
-            :copyright "Copyright Hugo Duncan 2010. All rights reserved."
+            :copyright "Copyright Hugo Duncan 2010, 2011. All rights reserved."
             :web-src-dir "http://github.com/hugoduncan/criterium/blob/"
             :web-home "http://hugoduncan.github.com/criterium/" })
