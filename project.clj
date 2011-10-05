33f336cf821cf75bc13548ec077cd39ee19def5b
Hugo Duncan <hugo@hugoduncan.org>
2011-10-04 22:35:42 -0400

    Update to clojure 1.3.0, and project to 0.2.0

--- project.clj
+++ project.clj
@@ -1,10 +1,7 @@
-(defproject criterium "0.1.1-SNAPSHOT"
+(defproject criterium "0.2.0-SNAPSHOT"
   :description "Benchmarking library"
   :url "http://github.com/hugoduncan/criterium"
-  :dependencies [[org.clojure/clojure "1.2.0"]]
-  :dev-dependencies [[swank-clojure/swank-clojure "1.2.1"]
-                     [org.clojure/clojure-contrib "1.2.0"]
-                     [autodoc "0.7.1"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :autodoc {:name "Criterium"
             :description "A benchmarking library."
             :copyright "Copyright Hugo Duncan 2010, 2011. All rights reserved."
