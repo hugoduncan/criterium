0d6217e07b88328e2d98cc7335fd021dd0d8a939
Hugo Duncan <hugo@hugoduncan.org>
2012-04-24 09:46:48 -0400

    Updated project.clj, release notes and readme for 0.2.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject criterium "0.2.1-SNAPSHOT"
+(defproject criterium "0.2.1"
   :description "Benchmarking library"
   :url "http://github.com/hugoduncan/criterium"
   :dependencies [[org.clojure/clojure "1.3.0"]]
