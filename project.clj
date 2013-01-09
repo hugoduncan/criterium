785f77aad850a9aa704bcbdeb47308feff8adad1
Hugo Duncan <hugo@hugoduncan.org>
2013-01-09 09:40:20 -0500

    Add scm to project.clj

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "https://github.com/hugoduncan/criterium"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:url "git@github.com:hugoduncan/criterium.git"}
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :local-repo-classpath true)
