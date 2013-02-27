1d41b1330e290d41bccac813d916c44e4695fc46
Hugo Duncan <hugo@hugoduncan.org>
2013-02-26 21:56:08 -0500

    Add benchmarks for timing loop implementations
    In order to quantify the impact of several timing loop implementations, adds
    benchmarks to quantify them.  There seems to be no difference in the result
    between using an array, or a mutable place.

---
+++ benchmarks/criterium/implementation/array.clj
@@ -0,0 +1,37 @@
+(ns criterium.implementation.array
+  "Use an array to store sample results"
+  (:use criterium.core))
+
+;;; Array based storage of sample results
+(def ^:const max-obj-array-size 4)
+
+(defn execute-expr-core-timed-part-with-array
+  [n f ret-vals-arr]
+  (let [^objects arr ret-vals-arr
+        arr-size-1 (long (dec (count arr)))
+        init-j (rem (dec n) max-obj-array-size)]
+    (time-body
+     (loop [i (long (dec n))
+            j (long init-j)
+            v (f)]
+       (aset arr j v)
+       (if (pos? i)
+         (recur (unchecked-dec i)
+                (if (zero? j) arr-size-1 (unchecked-dec j))
+                (f)))))))
+
+(defn execute-expr-core-with-array
+  [n f reduce-with]
+  (let [arr-size (int (min max-obj-array-size n))
+        arr-size-1 (int (dec arr-size))
+        ret-vals-arr (object-array arr-size)
+        time-and-ret (execute-expr-core-timed-part-with-array n f ret-vals-arr)]
+    (loop [i (int arr-size-1)
+           v (aget ret-vals-arr i)]
+      (if (pos? i)
+        (recur (dec i) (reduce-with v (aget ret-vals-arr (dec i))))
+        (replace-ret-val-in-time-body-result time-and-ret v)))))
+
+(defn with-array [f]
+  (with-redefs [criterium.core/execute-expr-core execute-expr-core-with-array]
+    (f)))
--- profiles.clj
+++ profiles.clj
@@ -9,4 +9,16 @@
  :release
  {:plugins [[lein-set-version "0.2.1"]]
   :set-version
-  {:updates [{:path "README.md" :no-snapshot true}]}}}
+  {:updates [{:path "README.md" :no-snapshot true}]}}
+ :dev {:aliases {"impl-perf" ["with-profile" "+impl" " perforate" "--quick"]}}
+ :impl {:perforate
+        {:environments
+         [{:name :array
+           :namespaces [criterium.implementation]
+           :fixtures [criterium.implementation.array/with-array]}
+          {:name :volatile
+           :namespaces [criterium.implementation]
+           :fixtures [criterium.implementation.type/with-volatile]}
+          {:name :unsynchronized
+           :namespaces [criterium.implementation]
+           :fixtures [criterium.implementation.type/with-unsynchronized]}]}}}
