a1fb6aa6def1be66bd1c1f8641978628b844cfe2
Hugo Duncan <hugo@hugoduncan.org>
2025-01-11 23:38:44 -0500

    feat: add sampled-fn

---
+++ bases/criterium/src/criterium/util/t_digest.clj
@@ -0,0 +1,28 @@
+(ns criterium.util.t-digest
+  (:require
+   [criterium.util.t-digest.merging-digest :as md]))
+
+(defn new-digest []
+  (md/new-digest))
+
+(defn add-point
+  "Add a single value into the digest"
+  [digest value]
+  (md/add-point digest value))
+
+(defn compress
+  "Merge any buffered points into the digest."
+  [digest]
+  (md/compress digest))
+
+(defn quantile
+  "Return estimated value at given quantile [0,1].
+   Return nil if digest is empty."
+  ^double [digest ^double x]
+  (md/cdf digest x))
+
+(defn cdf
+  "Return the cumulative probability at x.
+   Return NaN if digest is empty."
+  ^double [digest ^double x]
+  (md/cdf digest x))
