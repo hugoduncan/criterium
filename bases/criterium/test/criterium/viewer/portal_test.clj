e459652272f357ae5ca40044d54c0b3e16a7d4d9
Hugo Duncan <hugo@hugoduncan.org>
2025-01-12 21:18:46 -0500

    chore: fix portal-test

--- bases/criterium/test/criterium/viewer/portal_test.clj
+++ bases/criterium/test/criterium/viewer/portal_test.clj
@@ -28,37 +28,39 @@
 (deftest portal-samples-test
   (testing "portal-samples"
     (testing "charts the sample data"
-      (let [[chart] (with-tap-out
-                      (view/samples*
-                       {}
-                       (-> (test-data/samples-with-2-values-map)
-                           (assoc :viewer :portal))))]
+      (let [[title chart] (with-tap-out
+                            (view/samples*
+                             :portal
+                             {}
+                             (:data (test-data/samples-with-2-values-map))))]
         (is (= [{:elapsed-time 1.0, :index 0, :outlier ""}
                 {:elapsed-time 1.0, :index 1, :outlier ""}]
-               (-> chart :vconcat first :layer first :data :values)))))
+               (-> chart :vconcat first :layer first :data :values)))
+        (is (= [:b "Samples"] title))))
     (testing "charts with transformed sample data"
-      (let [[chart] (with-tap-out
-                      (view/samples*
-                       {}
-                       (-> (test-data/samples-with-transformed-values-map)
-                           (assoc :viewer :portal))))]
+      (let [[title chart] (with-tap-out
+                            (view/samples*
+                             :portal
+                             {}
+                             (:data
+                              (test-data/samples-with-transformed-values-map))))]
         (is (= [{:elapsed-time 1.0, :index 0, :outlier ""}
                 {:elapsed-time 2.0, :index 1, :outlier ""}
                 {:elapsed-time 4.0, :index 2, :outlier ""}]
-               (-> chart :vconcat first :layer first :data :values)))))
+               (-> chart :vconcat first :layer first :data :values)))
+        (is (= [:b "Samples"] title))))
     (testing "charts the sample data"
-      (let [bench-map (-> (test-data/samples-with-outliers-values-map)
-                          (assoc :viewer :portal))
-            quantiles (analyse/quantiles {:quantiles [0.9 0.99 0.99]})
-            outliers  (analyse/outliers)
-            stats     (analyse/stats)
-            view      (view/samples)
-            [chart]   (with-tap-out
-                        (-> bench-map
-                            quantiles
-                            outliers
-                            stats
-                            view))]
+      (let [bench-map     (:data (test-data/samples-with-outliers-values-map))
+            quantiles     (analyse/quantiles {:quantiles [0.9 0.99 0.99]})
+            outliers      (analyse/outliers)
+            stats         (analyse/stats)
+            view          (view/samples)
+            [title chart] (with-tap-out
+                            (->> bench-map
+                                 quantiles
+                                 outliers
+                                 stats
+                                 (view :portal)))]
         (is (= [{:elapsed-time 9.0, :index 0, :outlier ""}
                 {:elapsed-time 10.0, :index 1, :outlier ""}
                 {:elapsed-time 9.0, :index 2, :outlier ""}
@@ -66,113 +68,118 @@
                 {:elapsed-time 9.0, :index 4, :outlier ""}
                 {:elapsed-time 10.0, :index 5, :outlier ""}
                 {:elapsed-time 10000.0, :index 6, :outlier :high-severe}]
-               (-> chart :vconcat first :layer first :data :values)))))))
+               (-> chart :vconcat first :layer first :data :values)))
+        (is (= [:b "Samples"] title))))))
 
 (deftest portal-sample-percentiles-test
   (testing "portal-percentiles"
     (testing "charts the sample data"
-      (let [[chart] (with-tap-out
-                      (view/sample-percentiles*
-                       {}
-                       (-> (test-data/samples-with-2-values-map)
-                           (assoc :viewer :portal))))]
+      (let [[title chart] (with-tap-out
+                            (view/sample-percentiles*
+                             :portal
+                             {}
+                             (:data (test-data/samples-with-2-values-map))))]
         (is (= [{:elapsed-time 1.0, :x 0.0, :p 0}
                 {:elapsed-time 1.0, :x 1.0, :p 100.0}]
-               (-> chart :vconcat first :layer first :data :values)))))))
+               (-> chart :vconcat first :layer first :data :values)))
+        (is (= [:b "Percentiles"] title))))))
 
 (deftest portal-histogram-test
   (testing "portal-histogram"
     (testing "charts the sample data"
-      (let [bench-map
-            (-> (test-data/samples-with-2-values-map)
-                (assoc :viewer :portal))
+      (let [data-map
+            (:data (test-data/samples-with-2-values-map))
             quantiles      (analyse/quantiles {:quantiles [0.9 0.99 0.99]})
             outliers       (analyse/outliers)
             stats          (analyse/stats)
             view-histogrem (view/histogram)
-            [chart]        (with-tap-out
-                             (-> bench-map
-                                 quantiles
-                                 outliers
-                                 stats
-                                 view-histogrem))]
+            [title chart]  (with-tap-out
+                             (->> data-map
+                                  quantiles
+                                  outliers
+                                  stats
+                                  (view-histogrem :portal)))]
         (is (= [{:elapsed-time 1.0, :index 0, :outlier ""}
                 {:elapsed-time 1.0, :index 1, :outlier ""}]
-               (-> chart :vconcat first :layer first :data :values)))))))
+               (-> chart :vconcat first :layer first :data :values)))
+        (is (= [:b "Histogram"] title))))))
 
 (deftest portal-stats-test
   (testing "print-stats"
     (testing "prints via output-view"
-      (is (= [[{:metric            "Elapsed Time",
-                :mean              "100 ns",
-                :min-val           "89.0 ns",
-                :mean-minus-3sigma "88.0 ns",
-                :mean-plus-3sigma  "112 ns",
-                :max-val           "114 ns"}]]
+      (is (= [[:b "Summary stats"]
+              [{:_metric           "Elapsed Time ns",
+                :mean              100.0
+                :min-val           89.0
+                :mean-minus-3sigma 88.0
+                :mean-plus-3sigma  112.0
+                :max-val           114.0}]]
              (with-tap-out
                (view/stats*
+                :portal
                 {}
-                (-> (test-data/bench-stats-map)
-                    (assoc :viewer :portal))))))
+                (:data (test-data/bench-stats-map))))))
 
-      (is (= [[{:metric            "Elapsed Time",
-                :mean              "1.00 ns",
-                :min-val           "1.00 ns",
-                :mean-minus-3sigma "1.00 ns",
-                :mean-plus-3sigma  "1.00 ns",
-                :max-val           "1.00 ns"}]]
-             (let [bench-map
-                   (-> (test-data/samples-with-2-values-map)
-                       (assoc :viewer :portal))
+      (is (= [[:b "Summary stats"]
+              [{:_metric           "Elapsed Time ns",
+                :mean              1.00,
+                :min-val           1.00,
+                :mean-minus-3sigma 1.00,
+                :mean-plus-3sigma  1.00,
+                :max-val           1.00}]]
+             (let [data-map
+                   (:data (test-data/samples-with-2-values-map))
                    stats      (analyse/stats)
                    view-stats (view/stats)]
                (with-tap-out
-                 (-> bench-map
-                     stats
-                     view-stats))))))))
+                 (->> data-map
+                      stats
+                      (view-stats :portal)))))))))
 
 (deftest portal-outlier-count-test
   (testing "print-outlier-count"
     (testing "prints via view"
       (is
-       (= [[{:low-severe  0,
+       (= [[:b "Outliers"]
+           [{:low-severe  0,
              :low-mild    2,
              :high-mild   3,
              :high-severe 0,
-             :metric      "Elapsed Time"}]]
+             :_metric     "Elapsed Time"}]]
           (with-tap-out
             ((view/outlier-counts)
-             (-> (test-data/outlier-count-map)
-                 (assoc :viewer :portal)))))))))
+             :portal
+             (:data (test-data/outlier-count-map)))))))))
 
 (deftest portal-outlier-significance-test
   (testing "print-outlier-significance"
     (testing "prints via view"
-      (is (= [[{:effect :moderate :significance 0.25}]]
+      (is (= [[:b "Outlier Significance"]
+              [{:effect :moderate :significance 0.25}]]
              (with-tap-out
                ((view/outlier-significance)
-                (-> (test-data/outlier-significance-map)
-                    (assoc :viewer :portal)))))))))
+                :portal
+                (:data (test-data/outlier-significance-map)))))))))
 
 (deftest portal-event-stats-test
   (testing "print-event-stats"
     (testing "prints via report"
-      (is (= [[{:metric         "ClassLoader",
-                :sample-count   "1",
-                :loaded-count   "1",
-                :unloaded-count "1"}
+      (is (= [[:b "Event stats"]
+              [{:metric         "ClassLoader",
+                :sample-count   "1.0",
+                :loaded-count   "1.0",
+                :unloaded-count "1.0"}
                {:metric       "JIT compilation",
-                :sample-count "1",
+                :sample-count "1.0",
                 :time-ms      "3.00 ms"}
                {:metric             "Garbage Collector",
-                :total-sample-count "1",
-                :total-count        "2",
+                :total-sample-count "1.0",
+                :total-count        "2.0",
                 :total-time-ms      "1.00 ms"}]]
-             (let [bench-map   (-> (test-data/samples-for-event-stats-map)
-                                   (assoc :viewer :portal))
+             (let [data-map    (:data (test-data/samples-for-event-stats-map))
                    event-stats (analyse/event-stats)
                    view        (view/event-stats)]
                (with-tap-out
-                 (-> bench-map
-                     event-stats
-                     view))))))))
+                 (->> data-map
+                      event-stats
+                      (view :portal)))))))))
