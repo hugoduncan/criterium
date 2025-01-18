a95ad3d84e2298c10edff96f085ed38eb72bc168
Hugo Duncan <hugo@hugoduncan.org>
2025-01-18 18:39:51 -0500

    fix collect-plan-test

--- bases/criterium/test/criterium/collect_plan_test.clj
+++ bases/criterium/test/criterium/collect_plan_test.clj
@@ -4,7 +4,8 @@
    [criterium.collect-plan :as collect-plan]
    [criterium.collect-plan.config :as collect-plan-config]
    [criterium.collector :as collector]
-   [criterium.measured :as measured]))
+   [criterium.measured :as measured]
+   [criterium.types :as types]))
 
 (deftest one-shot-test
   (testing "one-shot"
@@ -14,17 +15,19 @@
           collector (collector/collector
                      {:stages     [:compilation :memory]
                       :terminator :elapsed-time})
-          sampled   (collect-plan/collect
+          data-map  (collect-plan/collect
                      (collect-plan-config/collect-plan-config
-                      :one-sot
+                      :one-shot
                       {})
                      collector
                      measured)]
-      (is (map? sampled))
-      (is (vector? (get (:samples sampled) [:elapsed-time])))
-      (is (= 1 (count (get (:samples sampled) [:elapsed-time]))))
-      (is (every? vector? (vals (:samples sampled))))
-      (is (= 1 (:expr-value sampled))))))
+      (is (map? data-map))
+      (is (types/collected-metrics-map? (:samples data-map)))
+      (is (vector? ((:metric->values (:samples data-map)) [:elapsed-time])))
+      (is (= 1
+             (count ((:metric->values (:samples data-map)) [:elapsed-time]))))
+      (is (every? vector? (vals (:metric->values (:samples data-map)))))
+      (is (= 1 (:expr-value (:samples data-map)))))))
 
 (deftest full-test
   (testing "full sampling"
@@ -33,14 +36,16 @@
                      (fn [_ _] [1000000 1]))
           collector (collector/collector {:stages     [:compilation :memory]
                                           :terminator :elapsed-time})
-          sampled   (collect-plan/collect
+          data-map  (collect-plan/collect
                      (collect-plan-config/collect-plan-config
                       :with-jit-warmup
                       {})
                      collector
                      measured)]
-      (is (map? sampled))
-      (is (vector? (get (:samples sampled) [:elapsed-time])))
-      (is (<= 10 (count (get (:samples sampled) [:elapsed-time]))))
-      (is (every? vector? (vals (:samples sampled))))
-      (is (= 1 (:expr-value sampled))))))
+      (is (map? data-map))
+      (is (types/collected-metrics-map? (:samples data-map)))
+      (is (vector? ((:metric->values (:samples data-map)) [:elapsed-time])))
+      (is (<= 10
+              (count ((:metric->values (:samples data-map)) [:elapsed-time]))))
+      (is (every? vector? (vals (:metric->values (:samples data-map)))))
+      (is (= 1 (:expr-value (:samples data-map)))))))
