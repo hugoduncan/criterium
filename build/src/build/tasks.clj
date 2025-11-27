37d22e2be9d6a6c3e614d09c9a558e7ece9d047a
Hugo Duncan <hugo@hugoduncan.org>
2025-11-27 17:33:52 -0500

    feat(ci): add clj-kondo config caching and source-paths utility
    - Add clj-kondo config caching step before linting
    - Create source-paths build function to collect all source/test paths
      from project and :local/root dependencies recursively
    - Use bases/projects for linting (simpler and more reliable)
    
    The source-paths function can be used for other tools that need to
    discover all source paths in the polylith monorepo.
    
    Usage: clojure -T:build source-paths

--- build/src/build/tasks.clj
+++ build/src/build/tasks.clj
@@ -1,6 +1,8 @@
 (ns build.tasks
   (:require [babashka.fs :as fs]
-            [build.common :as common]))
+            [build.common :as common]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
 (defn clean
       "Remove all built files."
@@ -59,3 +61,62 @@
       (compile-fn params)
       (fs/with-temp-dir [class-dir {:prefix "buildjavac"}]
         (compile-fn (assoc params :class-dir (str class-dir)))))))
+
+(defn- read-deps-edn
+  "Read and parse deps.edn file."
+  [path]
+  (when (.exists (io/file path))
+    (edn/read-string (slurp path))))
+
+(defn- get-local-root-deps
+  "Extract :local/root dependencies from deps.edn aliases."
+  [deps-edn alias-keys]
+  (let [aliases (select-keys (:aliases deps-edn) alias-keys)]
+    (->> aliases
+         vals
+         (mapcat :extra-deps)
+         (keep (fn [[_k v]] (when (:local/root v) (:local/root v))))
+         distinct)))
+
+(defn- get-paths-from-deps
+  "Get paths from a deps.edn file."
+  [deps-path]
+  (when-let [deps (read-deps-edn deps-path)]
+    {:paths (:paths deps)
+     :src-paths (get-in deps [:aliases :dev :extra-paths] [])
+     :test-paths (get-in deps [:aliases :test :extra-paths] [])}))
+
+(defn- collect-all-paths
+  "Recursively collect paths from project and local/root dependencies."
+  [root-dir alias-keys visited]
+  (let [deps-path (str root-dir "/deps.edn")]
+    (if (visited root-dir)
+      []
+      (when-let [deps (read-deps-edn deps-path)]
+        (let [local-deps (get-local-root-deps deps alias-keys)
+              {:keys [paths src-paths test-paths]} (get-paths-from-deps deps-path)
+              all-paths (concat paths src-paths test-paths)
+              ;; Make paths absolute
+              abs-paths (map #(str root-dir "/" %) all-paths)
+              ;; Recursively get paths from local deps
+              dep-paths (mapcat #(collect-all-paths
+                                  (str root-dir "/" %)
+                                  alias-keys
+                                  (conj visited root-dir))
+                                local-deps)]
+          (concat abs-paths dep-paths))))))
+
+(defn source-paths
+  "Return all source and test paths for the project and its :local/root dependencies.
+
+  Recursively walks through deps.edn to find all :local/root dependencies and
+  collects their :paths and paths from :dev and :test aliases.
+
+  Usage:
+    clojure -T:build source-paths
+
+  Returns: Space-separated paths printed to stdout."
+  [_params]
+  (let [paths (distinct (collect-all-paths "." [:dev :test] #{}))]
+    (println (clojure.string/join " " paths))
+    nil))
