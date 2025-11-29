09ce7848afa58b2e0df1879aa16d431a8e0018c7
Hugo Duncan <hugo@hugoduncan.org>
2025-11-29 13:44:53 -0500

    chore: hide more reflection warnings in dev

--- development/src/user.clj
+++ development/src/user.clj
@@ -5,6 +5,12 @@
   (require 'aero.alpha.core)
   (catch Exception _))
 (try
+  (require 'cider.nrepl.middleware.util.instrument)
+  (catch Exception _))
+(try
+  (require 'cider.nrepl.inlined.deps.toolsreader.v1v4v1.clojure.tools.reader)
+  (catch Exception _))
+(try
   (require 'clojure.test.check)
   (catch Exception _))
 (try
@@ -12,6 +18,9 @@
   (catch Exception _))
 (try
   (require 'clojure.tools.cli)
+  (catch Exception _))
+(try
+  (require 'clojure.tools.gitlibs)
   (catch Exception _))
 (try
   (require 'clojure.tools.reader)
@@ -28,9 +37,15 @@
   (require 'lambdaisland.deep-diff2)
   (catch Exception _))
 (try
+  (require 'malli.core)
+  (catch Exception _))
+(try
   (require 'nrepl.core)
   (require 'nrepl.middleware)
   (require 'nrepl.middleware.session)
+  (catch Exception _))
+(try
+  (require 'orchard.inspect)
   (catch Exception _))
 
 (alter-var-root #'*unchecked-math* (constantly :warn-on-boxed))
