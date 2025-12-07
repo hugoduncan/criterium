82322191963bf6cc3cd0f206c3f3079d48bedaee
Hugo Duncan <hugo@hugoduncan.org>
2025-12-07 11:24:11 -0500

    fix(build): use :name param directly for lib symbol in deploy
    The deploy task was expecting separate :group-id and :artifact-id params
    but project coordinates provide :name as the full maven coordinate string
    (e.g., "criterium/criterium"). This caused a NullPointerException when
    calling (name nil) on the missing :group-id.
    
    Now uses (symbol name) directly, matching the pattern in pom.clj.

--- build/src/build/tasks/deploy.clj
+++ build/src/build/tasks/deploy.clj
@@ -1,32 +1,22 @@
 (ns build.tasks.deploy
   (:require
-   [babashka.fs :as fs]
-   [build.common :as common]
    [build.verbose :as verbose]
    [clojure.tools.build.api :as b]
-   [clojure.tools.deps.util.dir :refer [with-dir]]
    [deps-deploy.deps-deploy :as dd]
    [taoensso.truss :refer [have]]))
 
 (defn deploy
-  [{:keys [basis jar-file pom-file project] :as params}]
-  (let [project-root (common/ensure-project-root "jar" project)
-        aliases      (with-dir
-                       (fs/file project-root)
-                       (common/get-project-aliases))
-        group-id     (:group-id params)
-        artifact-id  (:name params)
-        lib          (symbol (name group-id) (name artifact-id))
-        basis        (b/create-basis)
-        opts         (merge
-                      params
-                      {:basis      basis
-                       :lib        lib
-                       :classifier nil
-
-                       :installer :remote
-                       :artifact  jar-file
-                       :pom-file  (str (have pom-file))})]
+  [{:keys [jar-file pom-file name] :as params}]
+  (let [lib (symbol name)
+        basis (b/create-basis)
+        opts (merge
+              params
+              {:basis basis
+               :lib lib
+               :classifier nil
+               :installer :remote
+               :artifact jar-file
+               :pom-file (str (have pom-file))})]
     (verbose/println
      opts
      "Deploy jar"
