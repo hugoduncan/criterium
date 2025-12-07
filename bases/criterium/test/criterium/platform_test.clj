129fbb4604107f1f52620021297d9aa8c4f81d58
Hugo Duncan <hugo@hugoduncan.org>
2025-12-06 23:27:04 -0500

    fix: update platform-test for new point-estimates return format
    platform-point-estimates now returns a vector of maps with :name,
    :min-ns, and :mean-ns keys instead of a flat keyword->double map.

--- bases/criterium/test/criterium/platform_test.clj
+++ bases/criterium/test/criterium/platform_test.clj
@@ -4,13 +4,20 @@
    [criterium.platform :as platform]))
 
 (deftest platform-point-estimates-test
+  ;; Verifies that platform-point-estimates returns timing characterisation data
+  ;; for the platform with expected structure.
   (testing "platform-point-estimates"
-    (let [res (platform/platform-point-estimates {:limit-time-s 0.001})]
-      (is (= [:latency
-              :granularity
-              :constant-long
-              :constant-double
-              :constant-object
-              :constant-nil]
-             (keys res)))
-      (is (every? double? (vals res))))))
+    (testing "returns a vector of stat maps"
+      (let [res (platform/platform-point-estimates {:limit-time-s 0.001})]
+        (is (= ["latency"
+                "granularity"
+                "constant-long"
+                "constant-double"
+                "constant-object"
+                "constant-nil"]
+               (mapv :name res)))
+        (is (every? #(and (contains? % :min-ns)
+                          (contains? % :mean-ns))
+                    res))
+        (is (every? #(double? (:min-ns %)) res))
+        (is (every? #(double? (:mean-ns %)) res))))))
