6e981642415da5c53c48405d27a41120fc264fa2
Hugo Duncan <hugo@hugoduncan.org>
2025-12-17 08:58:56 -0500

    chore: fix some boxed warnings

--- bases/criterium/src/criterium/util/kde.clj
+++ bases/criterium/src/criterium/util/kde.clj
@@ -6,7 +6,7 @@
 (defn gamma-fn
   "Returns Gamma(z + 1 = number) using Lanczos approximation.
   Taken from rosettacode."
-  [number]
+  ^double [^double number]
   (if (< number 0.5)
     (/ Math/PI (* (Math/sin (* Math/PI number))
                   (gamma-fn (- 1 number))))
@@ -14,11 +14,14 @@
           c [0.99999999999980993 676.5203681218851 -1259.1392167224028
              771.32342877765313 -176.61502916214059 12.507343278686905
              -0.13857109526572012 9.9843695780195716e-6 1.5056327351493116e-7]]
-      (* (Math/sqrt (* 2 Math/PI))
+      (* (Math/sqrt (* 2.0 Math/PI))
          (Math/pow (+ n 7 0.5) (+ n 0.5))
          (Math/exp (- (+ n 7 0.5)))
-         (+ (first c)
-            (apply + (map-indexed #(/ %2 (+ n %1 1)) (next c))))))))
+         (+ (double (first c))
+            (double
+             (reduce
+              +
+              (map-indexed #(/ (double %2) (+ n (long %1) 1)) (next c)))))))))
 
 (comment
   (gamma-fn 1)  ; 1
@@ -36,13 +39,18 @@
 
   Eq, 2.19"
   [nu gamma zeta theta z]
-  (let [tan-theta (Math/tan theta)
-        cos-theta (Math/cos theta)
+  (let [^double nu    nu
+        ^double gamma gamma
+        ^double zeta  zeta
+        ^double theta theta
+        ^double z     z
+        tan-theta     (Math/tan theta)
+        cos-theta     (Math/cos theta)
         ;; sin-theta (Math/sin theta)
-        a1        (/ (* zeta zeta) (* gamma gamma cos-theta cos-theta))
-        a2        (/ (* zeta zeta) (* gamma gamma cos-theta cos-theta))
-        e1        (+ a1 (* zeta (- z 1)))
-        e2        (+ a2 (* zeta (- 1 z)))]
+        a1            (/ (* zeta zeta) (* gamma gamma cos-theta cos-theta))
+        a2            (/ (* zeta zeta) (* gamma gamma cos-theta cos-theta))
+        e1            (+ a1 (* zeta (- z 1)))
+        e2            (+ a2 (* zeta (- 1 z)))]
     (* (Math/pow nu (- z 1))
        (Math/pow (/ 1 (* tan-theta tan-theta)) (* zeta (- z 1)))
        (/ (* (gamma-fn e1) (gamma-fn e2))
