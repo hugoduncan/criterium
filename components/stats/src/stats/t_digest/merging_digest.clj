1757eabaf275b19922899c9b759a6fc8014c00a1
Hugo Duncan <hugo@hugoduncan.org>
2026-01-02 07:45:14 -0500

    fix(stats): eliminate boxed math warnings in merging-digest
    Wrap reduce-double-vector results with (double ...) coercion in mean
    and variance functions. The interface var loses the primitive ^double
    return type hint, causing boxing when the result is used in division.

--- components/stats/src/stats/t_digest/merging_digest.clj
+++ components/stats/src/stats/t_digest/merging_digest.clj
@@ -419,11 +419,12 @@
 (defn mean
   ^double [{:keys [centroids] :as ^TDigest digest}]
   (let [sum-weights  (.total-weight digest)
-        weighted-sum (reduce-double-vector
-                      (fn ^double [^double acc ^Centroid centroid]
-                        (+ acc (* (.mean centroid) (.weight centroid))))
-                      0.0
-                      centroids)]
+        weighted-sum (double
+                      (reduce-double-vector
+                       (fn ^double [^double acc ^Centroid centroid]
+                         (+ acc (* (.mean centroid) (.weight centroid))))
+                       0.0
+                       centroids))]
     (/ weighted-sum sum-weights)))
 
 (defn variance
@@ -434,14 +435,15 @@
   (^double [{:keys [centroids] :as ^TDigest digest} ^double mean]
    (let [sum-weights (.total-weight digest)
          n-1         (dec sum-weights)
-         sum-squares (reduce-double-vector
-                      (fn ^double [^double acc ^Centroid centroid]
-                        (+ acc
-                           (* (.mean centroid)
-                              (.mean centroid)
-                              (.weight centroid))))
-                      0.0
-                      centroids)
+         sum-squares (double
+                      (reduce-double-vector
+                       (fn ^double [^double acc ^Centroid centroid]
+                         (+ acc
+                            (* (.mean centroid)
+                               (.mean centroid)
+                               (.weight centroid))))
+                       0.0
+                       centroids))
          e-x-squared (/ sum-squares sum-weights)]
      (* (/ sum-weights n-1)  ; Bessel's correction factor
         (- e-x-squared (* mean mean))))))
