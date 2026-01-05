4ad9f74406d1ff44af5af88ab1aee69439f8537b
Hugo Duncan <hugo@hugoduncan.org>
2026-01-04 19:49:34 -0500

    fix(sampled-stats): allow empty quantiles config
    Remove assertion requiring :quantiles key in config since the
    quantiles-for function already provides defaults via
    (into #{0.1 0.25 0.5 0.75 0.9} (:quantiles config)).
    
    This allows using :quantiles as a bare keyword in analyse plans
    without specifying options.
    
    Fixes #555

--- bases/criterium/src/criterium/util/sampled_stats.clj
+++ bases/criterium/src/criterium/util/sampled_stats.clj
@@ -48,7 +48,6 @@
   {:pre [(have? seq path)
          (have? seq samples)
          (have? map? samples)]}
-  (have :quantiles config)
   (have (comp not :tail-quantile) config)
   (let [qs (vec (sort (into #{0.1 0.25 0.5 0.75 0.9} (:quantiles config))))
         vs (sort (samples-for-path samples path))]
