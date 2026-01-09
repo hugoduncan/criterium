558a35b39daef6952def1024d1ef8f4a41753c67
Hugo Duncan <hugo@hugoduncan.org>
2026-01-08 22:02:46 -0500

    feat(view): add domain-comparison-table and domain-comparison-chart multimethods
    Add new view multimethods for separate table and chart components of
    domain comparisons. Both include :none implementations.
    
    Part of story #630 to split domain-compare view components.

--- bases/criterium/src/criterium/view.clj
+++ bases/criterium/src/criterium/view.clj
@@ -70,6 +70,8 @@
 (def-multi-view domain-extract)
 (def-multi-view domain-grouped)
 (def-multi-view domain-comparison)
+(def-multi-view domain-comparison-table)
+(def-multi-view domain-comparison-chart)
 (def-multi-view domain-regression)
 (def-multi-view domain-apply)
 
@@ -123,5 +125,7 @@
 (defmethod domain-extract* :none [_ _ _])
 (defmethod domain-grouped* :none [_ _ _])
 (defmethod domain-comparison* :none [_ _ _])
+(defmethod domain-comparison-table* :none [_ _ _])
+(defmethod domain-comparison-chart* :none [_ _ _])
 (defmethod domain-regression* :none [_ _ _])
 (defmethod domain-apply* :none [_ _ _])
