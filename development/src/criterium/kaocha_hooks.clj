7e57f3d51a4a71f3d6f7ed812d55aaf96eddfbe4
Hugo Duncan <hugo@hugoduncan.org>
2026-01-12 11:53:39 -0500

    feat(test): add suppress-warnings-pre-load kaocha hook
    Add kaocha pre-load hook function that pre-loads third-party namespaces
    with reflection and boxed math warnings disabled. Replaces the previous
    standalone malli.generator binding with a general approach covering all
    noisy dependencies.

--- development/src/criterium/kaocha_hooks.clj
+++ development/src/criterium/kaocha_hooks.clj
@@ -1,17 +1,64 @@
 (ns criterium.kaocha-hooks
   "Kaocha hooks for criterium test runs.
 
-  Enables malli instrumentation before tests run to validate function
-  inputs and outputs at runtime.")
-
-;; Pre-require malli.generator with *unchecked-math* bound to nil
-;; to suppress boxed math warnings from malli.generator.cljc
-(binding [*unchecked-math* nil]
-  (try
-    (require 'malli.generator)
-    (catch Exception _)))
+  Provides hooks for:
+  - Malli instrumentation to validate function inputs and outputs
+  - Suppressing warnings from third-party libraries during test loading")
 
 (require '[criterium.schema.instrument :as inst])
+
+;;; Third-party namespaces with known reflection or boxed math warnings
+
+(def ^:private noisy-namespaces
+  "Third-party namespaces that emit reflection or boxed math warnings.
+  Each require wrapped in try/catch for silent failure when deps aren't on classpath."
+  '[aero.alpha.core
+    babashka.fs
+    cider.nrepl.inlined.deps.toolsreader.v1v4v1.clojure.tools.reader
+    cider.nrepl.middleware.test
+    cider.nrepl.middleware.util.instrument
+    clojure.test.check
+    clojure.test.check.clojure-test
+    clojure.tools.cli
+    clojure.tools.deps
+    clojure.tools.gitlibs
+    clojure.tools.reader
+    fipp
+    kaocha.plugin.profiling
+    kaocha.report
+    kaocha.runner
+    lambdaisland.deep-diff2
+    malli.core
+    malli.generator
+    nextjournal.beholder
+    nextjournal.markdown.transform
+    nextjournal.markdown.utils
+    nrepl.core
+    nrepl.middleware
+    nrepl.middleware.session
+    orchard.inspect
+    scicloj.clay.v2.make
+    scicloj.clay.v2.notebook
+    scicloj.clay.v2.util.image
+    scicloj.kindly-render.note.to-hiccup])
+
+(defn suppress-warnings-pre-load
+  "Pre-load hook that requires noisy third-party namespaces with warnings disabled.
+
+  Pre-loads namespaces that emit reflection or boxed math warnings before
+  the test suite's warning flags are in effect. Returns config unchanged.
+
+  Binds `*warn-on-reflection*` to false and `*unchecked-math*` to false
+  to suppress all warnings during loading. Each require is wrapped in
+  try/catch for silent failure when dependencies aren't on classpath."
+  [config]
+  (binding [*warn-on-reflection* false
+            *unchecked-math* false]
+    (doseq [ns-sym noisy-namespaces]
+      (try
+        (require ns-sym)
+        (catch Exception _))))
+  config)
 
 (defn instrument-pre-run
   "Pre-run hook that enables malli instrumentation.
