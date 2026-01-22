bc6d90c571ce276e4c285fbbfddfe5d5077be5a0
Hugo Duncan <hugo@hugoduncan.org>
2026-01-22 10:18:45 -0500

    feat(view): add extremes view multimethod
    Add def-multi-view for extremes along with the :none implementation.
    This view will show min/max values for quantitative metrics.

--- bases/criterium/src/criterium/view.clj
+++ bases/criterium/src/criterium/view.clj
@@ -38,6 +38,7 @@
 (def-multi-view samples)
 (def-multi-view stats)
 (def-multi-view shape-stats)
+(def-multi-view extremes)
 
 ;;; Distribution Fit Views
 
@@ -110,6 +111,7 @@
 (defmethod samples* :none [_ _ _])
 (defmethod stats* :none [_ _ _])
 (defmethod shape-stats* :none [_ _ _])
+(defmethod extremes* :none [_ _ _])
 
 ;; Distribution Fit Null Viewer
 (defmethod distribution-models* :none [_ _ _])
