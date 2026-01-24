558d65f9b8c9dc8bac347f6f82e64fb1abab73e1
Hugo Duncan <hugo@hugoduncan.org>
2026-01-24 15:20:09 -0500

    chore: remove duplicated function

--- components/array/src/criterium/array.clj
+++ components/array/src/criterium/array.clj
@@ -435,11 +435,6 @@
   [x]
   (instance? ITypedArray x))
 
-(defn long-array?
-  "Returns true if x is a LongArray."
-  [x]
-  (instance? LongArray x))
-
 (defn double-array?
   "Returns true if x is a DoubleArray."
   [x]
