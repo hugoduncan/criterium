1139a1414a52366fc309a42723faa008f4deab1b
Hugo Duncan <hugo@hugoduncan.org>
2026-01-27 11:37:32 -0500

    feat(array): add definterface+ macro with extends support
    Adds `definterface+` to `criterium.array.util` namespace that wraps
    `gen-interface` to support interface inheritance via `:extends` option,
    which Clojure's core `definterface` lacks.
    
    Syntax:
      (definterface+ IFoo [IBase IMarker] (^long method [^double x]))
    
    The optional first vector after the name specifies interfaces to extend.
    Without the vector, behaves identically to `definterface`.

---
+++ components/array/src/clj/criterium/array/util.clj
@@ -0,0 +1,38 @@
+(ns criterium.array.util
+  "Utilities for defining typed array interfaces.
+
+  Provides `definterface+`, an extension of Clojure's `definterface` that
+  supports interface inheritance via `gen-interface`'s `:extends` option.")
+
+(defmacro definterface+
+  "Create a new Java interface, optionally extending other interfaces.
+
+  Like `definterface`, but accepts an optional vector of interfaces to extend
+  as the first form after the name.
+
+  Examples:
+    ;; Without extends (same as definterface)
+    (definterface+ IFoo
+      (^long bar [^double x]))
+
+    ;; With single extend
+    (definterface+ IBar [IFoo]
+      (^double qux [^long y]))
+
+    ;; With multiple extends
+    (definterface+ IBaz [IFoo IBar]
+      (baz []))"
+  {:added "0.5"}
+  [name & forms]
+  (let [[extends sigs] (if (and (seq forms) (vector? (first forms)))
+                         [(first forms) (rest forms)]
+                         [nil forms])
+        tag            (fn [x] (or (:tag (meta x)) Object))
+        psig           (fn [[mname [& args]]]
+                         (vector mname (vec (map tag args)) (tag mname) (map meta args)))
+        cname          (with-meta (symbol (str (namespace-munge *ns*) "." name)) (meta name))]
+    `(let []
+       (gen-interface :name ~cname
+                      ~@(when extends [:extends (vec (map resolve extends))])
+                      :methods ~(vec (map psig sigs)))
+       (import ~cname))))
---
+++ components/array/test/criterium/array/util_test.clj
@@ -0,0 +1,111 @@
+(ns criterium.array.util-test
+  ;; Tests the definterface+ macro which extends definterface to support
+  ;; interface inheritance via gen-interface's :extends option.
+  ;; Contracts: macro without extends behaves like definterface,
+  ;; macro with single extend, macro with multiple extends,
+  ;; type hints propagate correctly through interface hierarchies.
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [criterium.array.util :refer [definterface+]]))
+
+;;; Test interface definitions
+
+(definterface+ ITestBase
+  (^long baseMethod [^double x]))
+
+(definterface+ ITestExtendsSingle [ITestBase]
+  (^double singleMethod [^long y]))
+
+(definterface+ ITestAnotherBase
+  (anotherBaseMethod []))
+
+(definterface+ ITestExtendsMultiple [ITestBase ITestAnotherBase]
+  (^String multiMethod [^long a ^double b]))
+
+(definterface+ ITestNoMethods [ITestBase])
+
+;;; Test implementations
+
+(deftype TestBaseImpl []
+  ITestBase
+  (baseMethod [_ x] (long x)))
+
+(deftype TestSingleExtendImpl []
+  ITestExtendsSingle
+  (baseMethod [_ x] (long x))
+  (singleMethod [_ y] (double y)))
+
+(deftype TestMultiExtendImpl []
+  ITestExtendsMultiple
+  (baseMethod [_ x] (long x))
+  (anotherBaseMethod [_] :another)
+  (multiMethod [_ a b] (str a "-" b)))
+
+(deftype TestMarkerImpl []
+  ITestNoMethods
+  (baseMethod [_ x] (long x)))
+
+;;; Tests
+
+(deftest definterface+-without-extends-test
+  (testing "definterface+ without extends"
+    (testing "creates interface with method signatures"
+      (is (.isInterface ITestBase)))
+    (testing "implementation can be instantiated and method called"
+      (let [impl (TestBaseImpl.)]
+        (is (= 42 (.baseMethod impl 42.5)))))))
+
+(deftest definterface+-with-single-extends-test
+  (testing "definterface+ with single extends"
+    (testing "creates interface that extends parent"
+      (is (.isInterface ITestExtendsSingle))
+      (is (isa? ITestExtendsSingle ITestBase)))
+    (testing "implementation satisfies both interfaces"
+      (let [impl (TestSingleExtendImpl.)]
+        (is (instance? ITestBase impl))
+        (is (instance? ITestExtendsSingle impl))))
+    (testing "both parent and child methods are callable"
+      (let [impl (TestSingleExtendImpl.)]
+        (is (= 10 (.baseMethod impl 10.5)))
+        (is (= 20.0 (.singleMethod impl 20)))))))
+
+(deftest definterface+-with-multiple-extends-test
+  (testing "definterface+ with multiple extends"
+    (testing "creates interface that extends all parents"
+      (is (.isInterface ITestExtendsMultiple))
+      (is (isa? ITestExtendsMultiple ITestBase))
+      (is (isa? ITestExtendsMultiple ITestAnotherBase)))
+    (testing "implementation satisfies all interfaces"
+      (let [impl (TestMultiExtendImpl.)]
+        (is (instance? ITestBase impl))
+        (is (instance? ITestAnotherBase impl))
+        (is (instance? ITestExtendsMultiple impl))))
+    (testing "all methods are callable"
+      (let [impl (TestMultiExtendImpl.)]
+        (is (= 5 (.baseMethod impl 5.0)))
+        (is (= :another (.anotherBaseMethod impl)))
+        (is (= "10-3.14" (.multiMethod impl 10 3.14)))))))
+
+(deftest definterface+-marker-interface-test
+  (testing "definterface+ with extends but no methods (marker interface)"
+    (testing "creates marker interface extending parent"
+      (is (.isInterface ITestNoMethods))
+      (is (isa? ITestNoMethods ITestBase)))
+    (testing "implementation satisfies both interfaces"
+      (let [impl (TestMarkerImpl.)]
+        (is (instance? ITestBase impl))
+        (is (instance? ITestNoMethods impl))))
+    (testing "parent method is callable"
+      (let [impl (TestMarkerImpl.)]
+        (is (= 99 (.baseMethod impl 99.0)))))))
+
+(deftest definterface+-type-hints-test
+  (testing "definterface+ type hints"
+    (testing "primitive return types work correctly"
+      (let [base-impl (TestBaseImpl.)
+            single-impl (TestSingleExtendImpl.)]
+        (is (= Long (type (.baseMethod base-impl 1.0))))
+        (is (= Double (type (.singleMethod single-impl 1))))))
+    (testing "object return types work correctly"
+      (let [impl (TestMultiExtendImpl.)]
+        (is (= String (type (.multiMethod impl 1 2.0))))))))
