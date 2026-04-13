dd9aecfda93261eb76f29866f316142e5638eedc
Hugo Duncan <hugo@hugoduncan.org>
2026-04-13 17:34:05 -0400

    fix(build): use :name param directly for lib symbol in deploy
    Commit 193bc8c8 (arg-gen) accidentally reverted the earlier fix from
    82322191. Project coordinates provide :name as the full maven coordinate
    string (e.g. "criterium/criterium") and no :group-id, so (name nil)
    threw NullPointerException during Clojars deploy.
    
    Restore (symbol name), matching the pattern used in pom.clj.

--- build/src/build/tasks/deploy.clj
+++ build/src/build/tasks/deploy.clj
@@ -1,29 +1,19 @@
 (ns build.tasks.deploy
   (:require
-   [babashka.fs :as fs]
-   [build.common :as common]
    [build.verbose :as verbose]
    [clojure.tools.build.api :as b]
-   [clojure.tools.deps.util.dir :refer [with-dir]]
    [deps-deploy.deps-deploy :as dd]
    [taoensso.truss :refer [have]]))
 
 (defn deploy
-  [{:keys [jar-file pom-file project] :as params}]
-  (let [project-root (common/ensure-project-root "jar" project)
-        _ (with-dir
-            (fs/file project-root)
-            (common/get-project-aliases))
-        group-id (:group-id params)
-        artifact-id (:name params)
-        lib (symbol (name group-id) (name artifact-id))
+  [{:keys [jar-file pom-file name] :as params}]
+  (let [lib (symbol name)
         basis (b/create-basis)
         opts (merge
               params
               {:basis basis
                :lib lib
                :classifier nil
-
                :installer :remote
                :artifact jar-file
                :pom-file (str (have pom-file))})]
